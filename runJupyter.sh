1a7ede1d64c222d8991d80dcd746dc1ba376b4a4
guserav <erik.schule@web.de>
2020-05-29 12:00:03 +0200

    Small adjustment of gitignore and runJupyter

--- runJupyter.sh
+++ runJupyter.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p python3Packages.notebook python3Packages.matplotlib
+#! nix-shell -i bash -p python3Packages.notebook python3Packages.matplotlib python3Packages.scikitlearn
 
 jupyter-notebook --no-browser
